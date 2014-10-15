eafe81739ec669538328700b71fda54f1c36917d
dacr <crosson.david@gmail.com>
2014-10-15 22:49:32 +0200

    work in progress

--- src/main/scala/collectionlab/CollectionLab.scala
+++ src/main/scala/collectionlab/CollectionLab.scala
@@ -16,6 +16,8 @@
 
 package collectionlab
 
+import collection.IndexedSeq
+import collection.IndexedSeqLike
 
 trait Cell[T,+V] {
   val time:T
@@ -27,11 +29,32 @@ case class BasicCell(time:Long, value:Double) extends Cell[Long,Double]
 
 case class OtherCell(time:Long, value:Double, count:Long) extends Cell[Long,Double]
 
+//
+//object Series {
+//  def newBuilder[C <: Cell](name: String, tm: TimeModel, alias: String)(implicit builder: CellBuilder[C]): Builder[C, Series[C]] = {
+//    new VectorBuilder mapResult { x: Vector[C] => fromVector(name, tm, alias, x) }
+//  }
+//
+//  implicit def canBuildFrom[C <: Cell](implicit builder: CellBuilder[C]): CanBuildFrom[Series[_], C, Series[C]] =
+//    new CanBuildFrom[Series[_], C, Series[C]] {
+//      def apply(): Builder[C, Series[C]] = newBuilder("default", 1L, "default")
+//      def apply(from: Series[_]): Builder[C, Series[C]] = newBuilder(from.name, from.tm, from.alias)
+//    }
+//
+//}
 
-class Series[+C](val name:String, protected val backend:Vector[C]=Vector.empty) {
-  def +[B>:C](that:B):Series[B] = new Series[B](name, backend :+ that)
-  def ++[B>:C](that:Iterable[B]):Series[B] = new Series[B](name, backend ++ that)
-  def filter(cdt: C => Boolean) = new Series[C](name, backend.filter(cdt))
+class Series[+C](protected val backend:Vector[C]=Vector.empty)
+//  extends IndexedSeq[C]
+//  with IndexedSeqLike[C, Series[C]]
+{
+  
+//  import collection.mutable.Builder
+//  override protected[this] def newBuilder: Builder[C, Series[C]] = Series.newBuilder(name, tm, alias)
+
+  
+  def +[B>:C](that:B):Series[B] = new Series[B](backend :+ that)
+  def ++[B>:C](that:Iterable[B]):Series[B] = new Series[B](backend ++ that)
+  def filter(cdt: C => Boolean) = new Series[C](backend.filter(cdt))
   override def toString:String = {
     val max=5
     val name=getClass.getName().split("[.]").last
@@ -40,14 +63,19 @@ class Series[+C](val name:String, protected val backend:Vector[C]=Vector.empty) 
   }
 }
 
+class NamedSeries[+C](val name:String) extends Series[C]
+
 
 
 object CollectionLab {
   def main(args:Array[String]) {
-    val s = new Series[Cell[Long,Double]]("truc")
+    val ns = new NamedSeries[Cell[Long,Double]]("truc")
     
-    val x = s + BasicCell(10,32d) + BasicCell(15,6) + OtherCell(16,9,2)
+    val x = ns + BasicCell(10,32d) + BasicCell(15,6) + OtherCell(16,9,2)
     
-    println(x.filter(_.value < 32))
+    val fx = x.filter(_.value < 32)
+    
+    val as = ns ++ (new Series[Cell[Long,Double]]() + BasicCell(1,0))
+    
   }
 }
